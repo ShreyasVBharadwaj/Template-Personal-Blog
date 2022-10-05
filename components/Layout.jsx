476cfd04ca0da9d2e463282ab66efdc419b73363
ShreyasVBharadwaj <87666947+ShreyasVBharadwaj@users.noreply.github.com>
2022-10-05 20:05:22 +0530

    Add files via upload

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Navbar';
+import NewsletterSubscribe from './footer'
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    {children}
+    <div className=" flex flex-col ">
+      <NewsletterSubscribe />
+    </div>
+  </>
+);
+
+export default Layout;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  loader: "akamai",
+  path:"",
+  reactStrictMode: true,
+  eslint: {
+  ignoreDuringBuilds: true,
+}
+};
